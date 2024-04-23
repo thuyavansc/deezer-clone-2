24e04e80b04105cda192f9205b99cbd51bbb9ecc
Kingston <Kingston@DESKTOP-MVP19NT>
2024-04-23 22:46:33 +0530

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+
+import Magnify from "vue-material-design-icons/Magnify.vue";
+import Bell from "vue-material-design-icons/Bell.vue";
+</script>
+
+<template>
+  <h1 class="text-red-600">Hello World</h1>
+  <RouterView />
+</template>
+
+<style scoped></style>
